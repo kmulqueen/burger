a5dc87071cafc501d1c32f0d13f8772524b8fa98
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-08-26 15:52:56 -0600

    formatted

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,20 @@ const orm = require("../config/orm.js");
 const burger = {
     all: function(cb) {
         orm.selectAll("burgers", function(res) {
-               console.log("res", res)
+            console.log("burger.all results", res);
             cb(res);
          
         });
     },
     insert: function(cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function(res) {
+            console.log("burger.insertOne results", res);
             cb(res);
         });
     },
     update: function(objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function(res) {
+            console.log("burger.updateOne results", res);
             cb(res);
         });
     }
--- server.js
+++ server.js
@@ -5,13 +5,12 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
 const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
