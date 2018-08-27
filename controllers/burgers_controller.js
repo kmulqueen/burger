33f8482c7a314d4ad1e61e6d2555e226ebe3fd79
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-08-26 19:23:36 -0600

    trying to mess with api routes. They aren't displaying the json data when I type them in the address bar.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,6 +15,7 @@ router.get("/", function (req, res) {
 router.post("/api/burgers", function (req, res) {
     burger.insert(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
         res.json({
+            status: "OK",
             id: result.insertId
         });
     });
